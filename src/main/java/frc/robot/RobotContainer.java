31b7ae0a8ff2263fb885f832df794fd46c96992e
Breichen1 <reichenmiikka@gmail.com>
2024-03-03 00:20:41 -0600

    first commit

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,88 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import frc.robot.Constants.LauncherConstants;
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.commands.Autos;
+import frc.robot.commands.LaunchNote;
+import frc.robot.commands.PrepareLaunch;
+import frc.robot.subsystems.CANDrivetrain;
+import frc.robot.subsystems.CANLauncher;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final CANDrivetrain m_drivetrain = new CANDrivetrain();
+  private final CANLauncher m_launcher = new CANLauncher();
+
+  //Your controller may not be an Xbox controller, but this object should work no matter what.
+  private final CommandXboxController m_driverController =
+      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  private final CommandXboxController m_operatorController =
+      new CommandXboxController(OperatorConstants.kOperatorControllerPort);
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    // Configure the trigger bindings
+    configureBindings();
+
+  }
+
+  
+
+  /**
+   * Use this method to define your trigger->command mappings. Triggers can be created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * predicate, or via the named factories in {@link
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
+   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * joysticks}.
+   */
+  private void configureBindings() {
+   // Set the default command for the drivetrain to drive using the joysticks
+    m_drivetrain.setDefaultCommand(
+        new RunCommand(
+            () ->
+                m_drivetrain.arcadeDrive(
+                    -m_driverController.getLeftY(), -m_driverController.getRightX()),
+            m_drivetrain));
+
+    /*Create an inline sequence to run when the operator presses and holds the A (green) button. Run the PrepareLaunch
+     * command for 1 seconds and then run the LaunchNote command */
+    m_operatorController
+        .a()
+        .whileTrue(
+            new PrepareLaunch(m_launcher)
+                .withTimeout(LauncherConstants.kLauncherDelay)
+                .andThen(new LaunchNote(m_launcher))
+                .handleInterrupt(() -> m_launcher.stop()));
+
+    // Set up a binding to run the intake command while the operator is pressing and holding the
+    // left Bumper
+    m_operatorController.leftBumper().whileTrue(m_launcher.getIntakeCommand());
+
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An example command will be run in autonomous
+    return Autos.exampleAuto(m_drivetrain);
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/CANDrivetrain.java
@@ -0,0 +1,61 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import static frc.robot.Constants.DrivetrainConstants.*;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+/* This class declares the subsystem for the robot drivetrain if controllers are connected via CAN. Make sure to go to
+ * RobotContainer and uncomment the line declaring this subsystem and comment the line for PWMDrivetrain.
+ *
+ * The subsystem contains the objects for the hardware contained in the mechanism and handles low level logic
+ * for control. Subsystems are a mechanism that, when used in conjuction with command "Requirements", ensure
+ * that hardware is only being used by 1 command at a time.
+ */
+public class CANDrivetrain extends SubsystemBase {
+  /*Class member variables. These variables represent things the class needs to keep track of and use between
+  different method calls. */
+  DifferentialDrive m_drivetrain;
+
+  /*Constructor. This method is called when an instance of the class is created. This should generally be used to set up
+   * member variables and perform any configuration or set up necessary on hardware.
+   */
+  public CANDrivetrain() {
+    WPI_VictorSPX leftFront = new WPI_VictorSPX(kLeftFrontID);
+    WPI_VictorSPX leftRear = new WPI_VictorSPX(kLeftRearID);
+      WPI_VictorSPX rightFront = new WPI_VictorSPX(kRightFrontID);
+      WPI_VictorSPX rightRear = new WPI_VictorSPX(kRightRearID);
+
+      // Set the rear motors to follow the front motors.
+      leftRear.follow(leftFront);
+      rightRear.follow(rightFront);
+
+      // Invert the left side so both side drive forward with positive motor outputs
+      leftFront.setInverted(true);
+      rightFront.setInverted(false);
+
+      // Put the front motors into the differential drive object. This will control all 4 motors with
+      // the rears set to follow the fronts
+      m_drivetrain = new DifferentialDrive(leftFront, rightFront);
+    }
+  
+
+  /*Method to control the drivetrain using arcade drive. Arcade drive takes a speed in the X (forward/back) direction
+   * and a rotation about the Z (turning the robot about it's center) and uses these to control the drivetrain motors */
+  public void arcadeDrive(double speed, double rotation) {
+    m_drivetrain.arcadeDrive(speed, rotation);
+  }
+
+  @Override
+  public void periodic() {
+    /*This method will be called once per scheduler run. It can be used for running tasks we know we want to update each
+     * loop such as processing sensor data. Our drivetrain is simple so we don't have anything to put here */
+  }
+}
+
